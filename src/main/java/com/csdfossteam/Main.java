d935353d2902e32d4b3f65cc51a6f2b59f4ada70
MaanooAk <akritasak@gmail.com>
2018-10-27 15:42:15 +0300

    Added project stucture with placeholders

---
+++ src/main/java/com/csdfossteam/Main.java
@@ -0,0 +1,14 @@
+
+package com.csdfossteam;
+
+public final class Main {
+
+    private Main() {}
+
+    public static void main(String[] args) {
+
+        System.out.println("hangman");
+
+    }
+
+}
