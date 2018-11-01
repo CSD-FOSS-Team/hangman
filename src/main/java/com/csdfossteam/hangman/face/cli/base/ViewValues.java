75ea623a3e8073dd22ed7385f097536e68c4e15b
MaanooAk <akritasak@gmail.com>
2018-11-01 10:55:30 +0200

    Implemented the view values holder

--- src/main/java/com/csdfossteam/hangman/face/cli/base/ViewValues.java
+++ src/main/java/com/csdfossteam/hangman/face/cli/base/ViewValues.java
@@ -1,8 +1,14 @@
 
 package com.csdfossteam.hangman.face.cli.base;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * A collection of values to be used in the render of a View.
+ * <p>
+ * The object to string call is performed on the request of the value.
  *
  * @see View
  *
@@ -10,6 +16,42 @@ package com.csdfossteam.hangman.face.cli.base;
  */
 public class ViewValues {
 
-    // TODO implement
+    private final Map<String, Object> values;
+
+    public ViewValues() {
+        this.values = new HashMap<>();
+    }
+
+    public ViewValues set(String name, Object value) {
+
+        values.put(name, value);
+        return this;
+    }
+
+    /**
+     * Returns the value of the given name, or null if missing.
+     */
+    public String get(String name) {
+        return get(name, null);
+    }
+
+    /**
+     * Returns the value of the given name, or the given default value if missing.
+     * <p>
+     * The default value may be null, in that case the returned value can be null.
+     */
+    public String get(String name, String defaul) {
+
+        final Object object = values.getOrDefault(name, defaul);
+        return object == null ? null : object.toString();
+    }
+
+    public void unset(String name) {
+        values.remove(name);
+    }
+
+    public void unsetAll() {
+        values.clear();
+    }
 
 }
