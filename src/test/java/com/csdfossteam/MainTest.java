115d63f42e4f388a3210ce8be7cd2aaca2c1201f
MaanooAk <akritasak@gmail.com>
2018-11-01 10:06:44 +0200

    Added dummy test

---
+++ src/test/java/com/csdfossteam/MainTest.java
@@ -0,0 +1,18 @@
+
+package com.csdfossteam;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class MainTest {
+
+    @Test
+    public void dummyTest() throws Exception {
+        // here to test the ci system
+
+        assertEquals(2, 1 + 1);
+    }
+
+}
