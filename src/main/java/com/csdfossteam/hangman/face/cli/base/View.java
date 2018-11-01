ef530412ef8a1f1833b4a69eeadf525fdf7e99a4
MaanooAk <akritasak@gmail.com>
2018-11-01 11:03:48 +0200

    First implementation of the basic view

--- src/main/java/com/csdfossteam/hangman/face/cli/base/View.java
+++ src/main/java/com/csdfossteam/hangman/face/cli/base/View.java
@@ -1,6 +1,13 @@
 
 package com.csdfossteam.hangman.face.cli.base;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 /**
  * A text interface with placeholder for values.
  * <p>
@@ -29,6 +36,96 @@ package com.csdfossteam.hangman.face.cli.base;
  */
 public class View {
 
-    // TODO implement
+    // TODO define a escape sequence
+    // TODO allow for repeating names
+
+    protected final String content;
+    protected final String pattern;
+
+    protected final Map<String, Integer> arguments;
+
+    /**
+     * Creates a view by parsing the given string.
+     */
+    public View(String content) {
+        this.content = content;
+
+        arguments = new HashMap<>();
+
+        pattern = parse(content, arguments);
+    }
+
+    private static String parse(String content, Map<String, Integer> arguments) {
+        final StringBuffer sb = new StringBuffer();
+
+        final Pattern pattern = Pattern.compile("\\{(.*)\\}");
+        final Matcher matcher = pattern.matcher(content);
+
+        int index = 0;
+
+        while (matcher.find()) {
+
+            final int len = matcher.group(0).length();
+            final String key = matcher.group(1);
+
+            final String replacement = parseArgument(arguments, index, len, key);
+
+            matcher.appendReplacement(sb, replacement);
+
+            index++;
+        }
+        matcher.appendTail(sb);
+
+        return sb.toString();
+    }
+
+    private static String parseArgument(Map<String, Integer> arguments, int index, int len, String keyRaw) {
+
+        final String key = keyRaw.trim();
+
+        if (key.isEmpty()) throw new RuntimeException("Missing placeholder name");
+        if (arguments.containsKey(key)) throw new RuntimeException("Duplicate placeholder name");
+
+        arguments.put(key, index);
+
+        if (keyRaw.startsWith(" ")) {
+            return "%" + len + "s";
+        } else {
+            return "%-" + len + "s";
+        }
+
+    }
+
+    /**
+     * Returns the text representation of the view with the given values.
+     */
+    public final String text(ViewValues values) {
+
+        return String.format(pattern, args(values));
+    }
+
+    protected final Object[] args(ViewValues values) {
+
+        final Object[] args = new Object[arguments.size()];
+
+        for (final Entry<String, Integer> i : arguments.entrySet()) {
+            final String name = i.getKey();
+            final int index = i.getValue();
+
+            args[index] = values.get(name, "");
+        }
+
+        return args;
+    }
+
+    /**
+     * Prints the text representation of the view with the given values using the
+     * given console.
+     */
+    public void print(Console console, ViewValues values) {
+
+        // TODO use the console
+        System.out.println(text(values));
+    }
 
 }
